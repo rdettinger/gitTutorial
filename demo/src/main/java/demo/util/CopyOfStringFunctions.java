ef6aa0245d1a228ce26e61298ba6e5bd742f5a17
Richard Dettinger <rdettinger@hunhuhealthcare.com>
2014-04-22 14:15:50 -0500

    Add comments so we could figure out WTF these functions do. Either of the parts with String functions in them had any javadoc whatsoever.  It was a mess.  I added comments so you would not be confused.

--- demo/src/main/java/demo/util/CopyOfStringFunctions.java
+++ demo/src/main/java/demo/util/CopyOfStringFunctions.java
@@ -2,6 +2,15 @@ package demo.util;
 
 public class CopyOfStringFunctions {
 
+	/**
+	 * It seems appropriate that we have a comment that tells you that this will
+	 * service as a concatenation function for Java.  Not sure why they didn't 
+	 * provide one directly for you.  Sigh.
+	 * 
+	 * @param str1
+	 * @param str2
+	 * @return  String The combination of the two input parameters.
+	 */
 	public String concat(String str1, String str2) {
 		return str1 + str2;
 	}
--- demo/src/main/java/demo/util/StringFunctions.java
+++ demo/src/main/java/demo/util/StringFunctions.java
@@ -2,7 +2,17 @@ package demo.util;
 
 public class StringFunctions {
 
+	/**
+	 * It seems appropriate that we have a comment that tells you that this will
+	 * service as a concatenation function for Java.  Not sure why they didn't 
+	 * provide one directly for you.  Sigh.
+	 * 
+	 * @param str1
+	 * @param str2
+	 * @return  String The combination of the two input parameters.
+	 */
 	public String concat(String str1, String str2) {
+		// Bring str1 together with str2.   
 		return str1 + str2;
 	}
 }
