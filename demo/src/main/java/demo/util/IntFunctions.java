875649fe7f09d8f7cad3b05a84f7476628020a44
Richard Dettinger <rdettinger@hunhuhealthcare.com>
2014-04-22 16:09:50 -0500

    Updated functionality related to ints.

--- demo/src/main/java/demo/util/IntFunctions.java
+++ demo/src/main/java/demo/util/IntFunctions.java
@@ -10,7 +10,22 @@ public class IntFunctions {
 	public int subtractInts(int i1, int i2) {
 		return i1 - i2;
 	}
+
+	public int divideInts(int i1, int i2) {
+		return i1 / i2;
+	}
 	
+	public int multiplyInts(int i1, int i2) {
+		return i1 * i2;
+	}
+
+	/**
+	 * This is a special routine.
+	 * 
+	 * @param i1
+	 * @param i2
+	 * @return
+	 */
 	public int addInts(int i1, int i2) {
 		return i1 - i2;
 	}
